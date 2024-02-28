927ace2ce99cfafefeedab359c0035fc0cb3a244
Bruno Santos <brunolsantos152@gmail.com>
2024-02-27 21:22:32 -0300

    feat: adding funds

--- components/tasks/show/show-task.tsx
+++ components/tasks/show/show-task.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 
 import { useEffect, useState } from "react"
@@ -236,12 +237,12 @@ export function ShowTask({
         </div>
         {
           tags?.length > 0 && (
-            <div className="flex text-[11px] font-semibold text-[#505050]  md:text-[14px]">
+            <div className="flex text-[11px] font-semibold text-grey dark:text-light md:text-[14px]">
             <p className="">Tags:</p>
             <div className="flex flex-wrap gap-y-[10px] italic">
               {tags?.map?.((tag, index) => (
                   <p
-                    className="ml-1 border-b border-[#505050]"
+                    className="ml-1 border-b border-[#505050] dark:border-[#7F8DA3]"
                     key={index}
                   >
                     {tag.tag}
@@ -252,6 +253,24 @@ export function ShowTask({
           </div>
           )
         }
+        <div className="mt-[25px] flex text-[12px] font-medium text-grey dark:text-light lg:text-[16px]">
+          <div className="mr-[22px] flex">
+          <svg
+            className="mr-[10px] size-[22px]"
+            fill="currentColor"
+            viewBox="0 0 22 22"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 17.4152V19.25C0 20.7668 3.69531 22 8.25 22C12.8047 22 16.5 20.7668 16.5 19.25V17.4152C14.7254 18.6656 11.4812 19.25 8.25 19.25C5.01875 19.25 1.77461 18.6656 0 17.4152ZM13.75 5.5C18.3047 5.5 22 4.2668 22 2.75C22 1.2332 18.3047 0 13.75 0C9.19531 0 5.5 1.2332 5.5 2.75C5.5 4.2668 9.19531 5.5 13.75 5.5ZM0 12.9078V15.125C0 16.6418 3.69531 17.875 8.25 17.875C12.8047 17.875 16.5 16.6418 16.5 15.125V12.9078C14.7254 14.3687 11.477 15.125 8.25 15.125C5.02305 15.125 1.77461 14.3687 0 12.9078ZM17.875 13.3805C20.3371 12.9035 22 12.0184 22 11V9.16523C21.0031 9.86992 19.5379 10.3512 17.875 10.6477V13.3805ZM8.25 6.875C3.69531 6.875 0 8.41328 0 10.3125C0 12.2117 3.69531 13.75 8.25 13.75C12.8047 13.75 16.5 12.2117 16.5 10.3125C16.5 8.41328 12.8047 6.875 8.25 6.875ZM17.673 9.29414C20.2512 8.83008 22 7.91914 22 6.875V5.04023C20.4746 6.11875 17.8535 6.69883 15.0949 6.83633C16.3625 7.45078 17.2949 8.27578 17.673 9.29414Z"/>
+          </svg>
+            <p className="mr-[3px] flex items-center">
+              Available funds:
+            </p>{' '}
+            <span className="flex items-center text-[12px] font-bold text-[#000] dark:text-[#fff] lg:text-[16px]">
+              ${usdValue}
+            </span>
+          </div>
+        </div>
         <Tabs defaultValue="description">
           <TabsList>
             <TabsTrigger value="description">Description</TabsTrigger>

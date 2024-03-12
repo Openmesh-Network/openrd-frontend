e745878b152b91f5edbc37b6f02a27bf7b7bc7f4
Bruno Santos <brunolsantos152@gmail.com>
2024-03-12 11:40:00 -0300

    feat: ui remove borders

--- components/tasks/show/show-task-summary.tsx
+++ components/tasks/show/show-task-summary.tsx
@@ -128,13 +128,13 @@ export function ShowTaskSummary({
   "No description was provided."
 
   return (
-    <Card className={`flex justify-between gap-x-[10px] py-[20px] ${index !== 0 && 'rounded-none'} ${index === 0 && 'rounded-b-none'}`}>
+    <Card className={`flex justify-between gap-x-[10px] border-x-0 border-b-[2px] border-t-0 py-[20px] !shadow-none ${index !== 0 && 'rounded-none'} ${index === 0 && 'rounded-b-none'}`}>
       <div>
         <CardHeader>
           <div className="text-lg font-bold">
             {title ?? <Skeleton className="h-6 w-[250px] bg-white" />}
           </div>
-          <div className="max-h-[100px] overflow-hidden">
+          <div  className="max-h-[100px] overflow-hidden">
             <SanitizeHTML html={description} />
           </div>
         </CardHeader>

93e8dcbcfc88357ce42e4ae35f58384664c247a5
Bruno Laureano <brunolsantos152@gmail.com>
2024-03-26 21:57:40 -0300

    feat: new mobile header

---
+++ components/mobile-nav.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable tailwindcss/no-unnecessary-arbitrary-value */
+import * as React from "react"
+import Image from "next/image"
+import Link from "next/link"
+
+import { NavItem } from "@/types/nav"
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from "react"
+import { NotificationsToggle } from "./notifications-toggle"
+import { ThemeToggle } from "./theme-toggle"
+
+interface MainNavProps {
+  items?: NavItem[]
+}
+
+export function MobileNav({ items }: MainNavProps) {
+  const pathname = usePathname()
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  
+  function navbarToggleHandler() {
+    setNavbarOpen(!navbarOpen)
+  }
+
+  return (
+    <div className="">
+        <button
+            onClick={navbarToggleHandler}
+            id="navbarToggler"
+            aria-label="Mobile Menu"
+            className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
+        >
+            <span
+                className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300  ${
+                navbarOpen ? ' top-[7px] rotate-45' : ' '
+                }`}
+            />
+            <span
+                className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${
+                navbarOpen ? 'opacity-0 ' : ' '
+                }`}
+            />
+            <span
+                className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300  ${
+                navbarOpen ? ' top-[-8px] -rotate-45' : ' '
+                }`}
+            />
+        </button>
+        <nav
+            id="navbarCollapse"
+            className={`navbar border-body-color/50 dark:border-body-color/20 dark:bg-dark absolute right-0 z-50 w-full rounded border-[.5px] bg-white px-6 py-4 duration-300 lg:visible lg:static lg:mr-[95px] lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
+            navbarOpen
+                ? 'visibility top-full opacity-100'
+                : 'invisible top-[120%] opacity-0'
+            }`}
+        >
+            <ul className="grid gap-y-[10px] pb-[19.5px] pt-[17px] md:gap-x-[12px] md:pb-[23.5px] md:pt-[20.5px]  lg:flex lg:gap-x-[22px] lg:pb-[27px] lg:pt-[24px] xl:gap-x-[32px] xl:pb-[31px] xl:pt-[27px] 2xl:gap-x-[65px] 2xl:pb-[39px] 2xl:pt-[34px]">
+                {items?.map(
+                    (item, index) =>
+                    item.href && (
+                        <Link
+                        key={index}
+                        href={item.href}
+                        className={cn(
+                            "flex items-center text-base font-medium text-muted-foreground hover:text-[#000000d0] dark:hover:text-[#ffffffdc]",
+                            item.disabled && "cursor-not-allowed opacity-80",
+                            pathname.endsWith('/tasks') && item.title === 'Tasks' && "text-black dark:text-white",
+                            pathname.endsWith('/') && item.title === 'Home' && "text-black dark:text-white",
+                        )}
+                        >
+                        {item.title}
+                        </Link>
+                    )
+                )}
+                <li className="mt-2">
+                    <NotificationsToggle />
+                    <ThemeToggle />
+                    <w3m-button />
+                </li>
+            </ul>
+    </nav>
+  </div>
+  )
+}
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,17 +3,27 @@
 import { useEffect, useState } from "react"
 import { useAccount } from "wagmi"
 
+import Image from "next/image"
+import Link from "next/link"
+
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 import { NotificationsToggle } from "./notifications-toggle"
+import { MobileNav } from "./mobile-nav"
 
 export function SiteHeader() {
   const [navItems, setNavItems] = useState<NavItem[]>(siteConfig.mainNav)
+  const [navbarOpen, setNavbarOpen] = useState(false)
 
   const account = useAccount()
+
+  function navbarToggleHandler() {
+    setNavbarOpen(!navbarOpen)
+  }
+
   useEffect(() => {
     let items = [...siteConfig.mainNav]
     if (account.isConnected && account.address) {
@@ -27,7 +37,7 @@ export function SiteHeader() {
 
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container h-16 items-center space-x-4 sm:justify-between sm:space-x-0 hidden md:flex">
         <MainNav items={navItems} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
@@ -36,6 +46,13 @@ export function SiteHeader() {
             <w3m-button />
           </nav>
         </div>
+      </div>
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0 md:hidden">
+        <Link href="/" className="flex items-center space-x-2">
+          <Image alt="Logo" src="/icon.png" width={20} height={20} />
+          <span className="inline-block font-bold">{siteConfig.name}</span>
+        </Link>
+        <MobileNav items={navItems} />
       </div>
     </header>
   )

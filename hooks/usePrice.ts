ef87428c1825630f359a5ba703e27340f9d25b62
Plopmenz <plopmenz@gmail.com>
2024-05-02 10:01:43 +0200

    remove API call for get price, do a client call instead

--- hooks/usePrice.ts
+++ hooks/usePrice.ts
@@ -1,36 +1,38 @@
 import { useEffect, useState } from "react"
 import { Task } from "@/openrd-indexer/types/tasks"
-import { replacer } from "@/openrd-indexer/utils/json"
-import axios from "axios"
+import { publicClients } from "@/openrd-indexer/utils/chain-cache"
+import { getPrice } from "@/openrd-indexer/utils/get-token-price"
+import { createPublicClient, http } from "viem"
 
-import { GetPriceRequest, GetPriceResponse } from "@/app/api/getPrice/route"
+import { chains } from "@/config/wagmi-config"
 
 export function usePrice({ chainId, task }: { chainId: number; task?: Task }) {
   const [price, setPrice] = useState<number | undefined>(undefined)
   useEffect(() => {
-    const getPrice = async () => {
+    const fetchPrice = async () => {
       if (!task) {
         setPrice(undefined)
         return
       }
 
-      const request: GetPriceRequest = {
-        chainId: chainId,
-        nativeBudget: task.nativeBudget,
-        budget: task.budget,
+      const chain = chains.find((c) => c.id === chainId)
+      if (!chain) {
+        return Response.json(
+          { error: `Chain with id ${chainId} not found.` },
+          { status: 404 }
+        )
       }
-      const newPrice = await axios
-        .post("/api/getPrice", JSON.parse(JSON.stringify(request, replacer)))
-        .then((res) => res.data as GetPriceResponse)
-        .then((res) => res.price)
-        .catch((err) => {
-          console.error(err)
-          return undefined
+      if (!publicClients[chain.id]) {
+        publicClients[chain.id] = createPublicClient({
+          chain: chain,
+          transport: http(),
         })
+      }
+      const newPrice = await getPrice(chain, task.nativeBudget, task.budget)
       setPrice(newPrice)
     }
 
-    getPrice().catch(console.error)
+    fetchPrice().catch(console.error)
   }, [chainId, task])
   return price
 }

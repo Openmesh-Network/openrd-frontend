6c7d0ebf2f595562fe7e545ca1f43a642a9ef8e1
Plopmenz <plopmenz@gmail.com>
2024-10-08 18:13:46 +0200

    dark layout hamburger menu

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -33,17 +33,17 @@ export function MobileNav({ items }: MainNavProps) {
         className="absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
       >
         <span
-          className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300  ${
+          className={`relative my-1.5 block h-0.5 w-[30px] bg-black dark:bg-white transition-all duration-300  ${
             navbarOpen ? " top-[7px] rotate-45" : " "
           }`}
         />
         <span
-          className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${
+          className={`relative my-1.5 block h-0.5 w-[30px] bg-black dark:bg-white transition-all duration-300 ${
             navbarOpen ? "opacity-0 " : " "
           }`}
         />
         <span
-          className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300  ${
+          className={`relative my-1.5 block h-0.5 w-[30px] bg-black dark:bg-white transition-all duration-300  ${
             navbarOpen ? " top-[-8px] -rotate-45" : " "
           }`}
         />
